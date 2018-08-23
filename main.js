bb8372463dcfc6dc902fbfae01733547acd3aede
Jessica <evasoserious@gmail.com>
2018-08-23 14:18:33 +0200

    wip

---
+++ main.js
@@ -0,0 +1,19 @@
+function init() {
+    Tabletop.init({
+      key: 'https://docs.google.com/spreadsheets/d/13NVWy6ScSoZWwEqACoWwSYwL5TnHlzsn6l5TXXpl6sQ/pubhtml',
+      
+      callback: function (tabletopData, tabletop) {
+        console.log(tabletopData)
+   
+        var app4 = new Vue({
+          el: '#app-4',
+          data: {
+            list: tabletopData,
+          }
+        })
+      },
+      simpleSheet: true
+    })
+   }
+   
+   window.addEventListener('DOMContentLoaded', init)
